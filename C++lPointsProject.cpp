302aaa3d3a1346a4f5da214502cdf2e9f51cc635
johco178 <johco178@student.otago.ac.nz>
2024-08-16 15:51:56 +1200

    Add project files.

---
+++ C++lPointsProject.cpp
@@ -0,0 +1,12 @@
+﻿// C++lPointsProject.cpp : Defines the entry point for the application.
+//
+
+#include "C++lPointsProject.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
