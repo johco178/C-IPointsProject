09869c1bb78b42b357000e183b97efdef567a6d0
Curtis Mellsop <melcu961@student.otago.ac.nz>
2024-08-28 11:57:08 +1200

    Added TicTacToe game

---
+++ TicTacToe.h
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+const int ROWS = 3;
+const int COLS = 3;
+extern char board[ROWS][COLS];
+
+void initBoard();
+void displayBoard();
+bool checkWin();
+bool checkDraw();
+void move(char player);
