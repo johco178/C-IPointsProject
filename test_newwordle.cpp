6a772aca8eef7fa8db5776ab84c9c8b568d6166c
johco178 <johco178@student.otago.ac.nz>
2024-08-29 09:11:11 +1200

    making test for wordle not finished

--- test_newwordle.cpp
+++ test_newwordle.cpp
@@ -6,3 +6,16 @@
 #include "newwordle.h"
 
 
+// Simple test framework
+#define TEST(name) static void test_##name(void)
+#define RUN_TEST(name) do { \
+    printf("Running %s...\n", #name); \
+    test_##name(); \
+    printf("%s passed!\n", #name); \
+} while (0)
+
+
+TEST(conatainsonlychar) {
+    const char* guess =  
+
+}
