fb8f073b6db0ff150c941dacc9bc591084f2f66b
Curtis Mellsop <melcu961@student.otago.ac.nz>
2024-08-29 10:15:47 +1200

    test function changes

--- test_tictactoe.cpp
+++ test_tictactoe.cpp
@@ -6,7 +6,6 @@ int main(){
     test_checkWin();
     test_checkDraw();
     return 0;
-
 }
 
 void test_initBoard(){
@@ -19,7 +18,7 @@ void test_initBoard(){
             }
         }
     }
-    std::cout << "Initiate board function: " << (pass ? "Passed" : "Failed") << std::endl;
+    std::cout << "Initiate board function: " << (pass ? "PASSED" : "FAILED") << std::endl;
 }
 
 void test_checkWin(){
@@ -28,21 +27,28 @@ void test_checkWin(){
     board[0][0] = 'X';
     board[0][1] = 'X';
     board[0][2] = 'X';
-    std::cout << "Check Win function, row test: " << (checkWin() ? "Passed" : "Failed") << std::endl;
+    std::cout << "Check Win function, row test: " << (checkWin() ? "PASSED" : "FAILED") << std::endl;
 
     // Check winning column
     initBoard();
     board[0][0] = 'O';
     board[1][0] = 'O';
     board[2][0] = 'O';
-    std::cout << "Check Win function, column test: " << (checkWin() ? "Passed" : "Failed") << std::endl;
+    std::cout << "Check Win function, column test: " << (checkWin() ? "PASSED" : "FAILED") << std::endl;
 
     // Check winning diagonal
     initBoard();
     board[0][0] = 'X';
     board[1][1] = 'X';
     board[2][2] = 'X';
-    std::cout << "Check Win function, diagonal test: " << (checkWin() ? "Passed" : "Failed") << std::endl;
+    std::cout << "Check Win function, diagonal test: " << (checkWin() ? "PASSED" : "FAILED") << std::endl;
+
+    // Check non-winning state
+    initBoard();
+    board[0][0] = 'X';
+    board[0][1] = '0';
+    board[0][2] = 'X';
+    std::cout << "Check Win function, non-winning state: " << (!checkWin() ? "PASSED" : "FAILED") << std::endl;
 
 
 }
@@ -59,6 +65,6 @@ void test_checkDraw(){
             board[i][j] = drawState[i][j];
         }
     }
-    std::cout << "Check Draw function: " << (checkDraw() ? "Passed" : "Failed") << std::endl;
+    std::cout << "Check Draw function: " << (checkDraw() ? "PASSED" : "FAILED") << std::endl;
 
 }
