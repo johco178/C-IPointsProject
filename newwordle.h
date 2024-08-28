f527bcc3aca4c2b076c14e6e1d6aa8432e6e9264
johco178 <johco178@student.otago.ac.nz>
2024-08-29 09:48:58 +1200

    finished first tests

--- newwordle.h
+++ newwordle.h
@@ -10,7 +10,7 @@
 
 void playWordle(void);
 bool validworda(const char* guess);
-bool processGuess(const char* theWord, const char* theGuess);
+char* processGuess(const char* theWord, const char* theGuess);
 char* toLowerCase(const char* guess);
 bool conatainsonlychar(const char* guess);
 bool correctlength(const char* guess);
--- test_newwordle.cpp
+++ test_newwordle.cpp
@@ -16,6 +16,46 @@
 
 
 TEST(conatainsonlychar) {
-    const char* guess = "d";
+
+    assert(conatainsonlychar("al-ha") == false);
+    assert(conatainsonlychar("alpha") == true);
 
 }
+TEST(toLowerCase) {
+
+    const char* guess = "HELLO";
+    char* lower = toLowerCase(guess);
+    assert(strcmp(lower, "hello") == 0);
+
+    const char* alreadylower = "hello";
+    char* lower = toLowerCase(alreadylower);
+    assert(strcmp(lower, "hello") == 0);
+}
+TEST(processGuess) {
+    const char* answer = "apple";
+    const char* guess = "anvil";
+    assert(strcmp(processGuess(answer, guess), "G___Y") == 0);
+
+    const char* guess = "piper";
+    assert(strcmp(processGuess(answer, guess), "Y_GY_") == 0);
+
+    const char* guess = "apple";
+    assert(strcmp(processGuess(answer, guess), "GGGGG") == 0);
+}
+
+
+
+
+
+
+int main() {
+    printf("Running Hangman unit tests...\n");
+
+    RUN_TEST(conatainsonlychar);
+    RUN_TEST(toLowerCase);
+    RUN_TEST(processGuess);
+
+    printf("All tests passed!\n");
+    return 0;
+}
+
