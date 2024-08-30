21c8129852e2639f7d3cf02b9a7715f21b1aa182
Rochelle Cole <colro338@otago.ac.nz>
2024-08-30 14:14:43 +1200

    doxy

--- html/search/all_3.js
+++ html/search/all_3.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['displayboard_0',['displayBoard',['../_tic_tac_toe_8cpp.html#a8e1ba472e6221f085694f519391d7dd4',1,'displayBoard():&#160;TicTacToe.cpp'],['../_tic_tac_toe_8h.html#a8e1ba472e6221f085694f519391d7dd4',1,'displayBoard():&#160;TicTacToe.cpp']]],
   ['divide_5fby_5ftwo_1',['divide_by_two',['../codecoveragetester_8h.html#aa72ff6417ef28b4c3bf2840c013c82f3',1,'codecoveragetester.h']]],
-  ['doxygen_20link_2',['Doxygen Link',['../md__r_e_a_d_m_e.html#autotoc_md2',1,'']]],
-  ['doxygen_20main_20page_3',['VintageVault Doxygen Main page',['../index.html',1,'']]]
+  ['doxygen_20main_20page_2',['VintageVault Doxygen Main page',['../index.html',1,'']]]
 ];
--- html/search/all_7.js
+++ html/search/all_7.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['link_0',['Doxygen Link',['../md__r_e_a_d_m_e.html#autotoc_md2',1,'']]],
-  ['load_5fdictionary_1',['load_dictionary',['../hangman_8cpp.html#a4c89622c29199a348cfa169b60745b19',1,'hangman.cpp']]],
-  ['lpointsproject_2',['C++lPointsProject',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['load_5fdictionary_0',['load_dictionary',['../hangman_8cpp.html#a4c89622c29199a348cfa169b60745b19',1,'hangman.cpp']]],
+  ['lpointsproject_1',['C++lPointsProject',['../md__r_e_a_d_m_e.html',1,'']]]
 ];
