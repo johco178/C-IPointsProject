fafc690ae1e637d8de822823a2f45c0a2911fc36
Nick Garner <garni988@student.otago.ac.nz>
2024-09-19 11:16:32 +1200

    pls

--- test_hangman.cpp
+++ test_hangman.cpp
@@ -76,7 +76,7 @@ TEST(print_hangman) {
     print_hangman(0);
     std::string output = buffer.str();
     std::cout << "Actual output for print_hangman(0):\n" << output << std::endl;
-    assert(output.find("  +---+") != std::string::npos);
+    assert(output.find(" +---+") != std::string::npos);
 
     buffer.str("");
     print_hangman(6);
