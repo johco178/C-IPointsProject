7b5b77b3d39dbbab09600bd979195d866e96aa65
CurtisM48 <CurtisM48@users.noreply.github.com>
2024-09-18 23:57:22 +0000

    deploy: 53ff07def7bc4407b7b90be06c4fc0749243f8df

--- search/functions_2.js
+++ search/functions_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['getdifficulty_0',['getDifficulty',['../secretNumber_8h.html#a71d953cc6e73849e18db29f5920cdd6e',1,'secretNumber.cpp']]],
+  ['getdifficulty_0',['getDifficulty',['../secretNumber_8h.html#afb2ef26556b9e05109ec74a74a94c621',1,'secretNumber.cpp']]],
   ['getguess_1',['getGuess',['../secretNumber_8h.html#a44a8d652c63ebe56e1ad6beb78497454',1,'secretNumber.cpp']]],
   ['getsecretnumber_2',['getSecretNumber',['../secretNumber_8h.html#a318f35b6d5a6e2387867b2aa93880df0',1,'secretNumber.cpp']]]
 ];
