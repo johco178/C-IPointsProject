5ec893cd4c9423d070db4bd6064593220bfb46c1
Nick Garner <garni988@student.otago.ac.nz>
2024-09-19 12:09:00 +1200

    oopsie

--- hangman.h
+++ hangman.h
@@ -80,7 +80,7 @@ void set_mock_dictionary(const char mock_dict[][MAX_WORD_LENGTH], int size);
 	 @return 1 if the dictionary was loaded successfully, 0 otherwise
 
 */
-STATIC int load_dictionary(const char* filename, char dictionary[][MAX_WORD_LENGTH], int max_size);
+int load_dictionary(const char* filename, char dictionary[][MAX_WORD_LENGTH], int max_size);
 #endif
 
 #endif // HANGMAN_H
