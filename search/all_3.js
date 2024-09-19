b9ec4dd4bb1790af19ca3b2d7bce0680c1efb7f6
colro338student <colro338student@users.noreply.github.com>
2024-09-19 13:59:04 +0000

    deploy: 52494f114b95d3daf16db73985e3f68d4d1804b8

--- search/all_3.js
+++ search/all_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['getdifficulty_0',['getDifficulty',['../secretNumber_8h.html#afb2ef26556b9e05109ec74a74a94c621',1,'secretNumber.cpp']]],
+  ['getdifficulty_0',['getDifficulty',['../secretNumber_8h.html#af7b4df3e82af155a8d9194fb4f74bbbb',1,'secretNumber.cpp']]],
   ['getguess_1',['getGuess',['../secretNumber_8h.html#a844e898309a8fbfee9f8e9ceb0ab21f0',1,'secretNumber.cpp']]],
   ['getsecretnumber_2',['getSecretNumber',['../secretNumber_8h.html#a318f35b6d5a6e2387867b2aa93880df0',1,'secretNumber.cpp']]]
 ];
