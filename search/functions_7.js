a70195422b4a29eb58c60206060a5e08e390a6f7
johco178 <johco178@users.noreply.github.com>
2024-10-09 23:02:53 +0000

    deploy: b841453f01b96c1845be9b9e54664c46dca94a3a

--- search/functions_7.js
+++ search/functions_7.js
@@ -4,7 +4,7 @@ var searchData=
   ['playagain_1',['playAgain',['../secretNumber_8h.html#ad39a7559b13b2473bc9bc3c97a45d1da',1,'secretNumber.cpp']]],
   ['playtictactoe_2',['playTicTacToe',['../TicTacToe_8cpp.html#aaef9cb7ffb3d32969b7db9a6374ca82c',1,'playTicTacToe():&#160;TicTacToe.cpp'],['../TicTacToe_8h.html#aaef9cb7ffb3d32969b7db9a6374ca82c',1,'playTicTacToe():&#160;TicTacToe.cpp']]],
   ['playwordle_3',['playWordle',['../newwordle_8h.html#ad2bee05444b1c1bcf1de4b58cb9d4935',1,'newwordle.cpp']]],
-  ['print_5fcolored_5foutput_4',['print_colored_output',['../newwordle_8h.html#a16ed7027e03ee215dcc09b15359ac0a9',1,'newwordle.cpp']]],
+  ['print_5fcolored_5foutput_4',['print_colored_output',['../newwordle_8h.html#acc010995d0958c735ed25be134d7897e',1,'newwordle.cpp']]],
   ['printhangman_5',['printHangman',['../hangman_8cpp.html#a9aa09b5014ad51f48c198c9ed4926fec',1,'hangman.cpp']]],
   ['printword_6',['printWord',['../hangman_8cpp.html#aeb646a8f33d77a74acfae9d9f7df4b59',1,'hangman.cpp']]],
   ['processguess_7',['processGuess',['../newwordle_8h.html#a11d73c9e69f098e07992a5c8706fe43a',1,'newwordle.cpp']]]
