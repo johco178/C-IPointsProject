102f54c2b6134acf491831e8bb21742b319bdd64
johco178 <johco178@users.noreply.github.com>
2024-10-10 23:31:20 +0000

    deploy: b2314238f2b75c73da7f0fb29d39c2d997a701d7

--- search/all_3.js
+++ search/all_3.js
@@ -1,7 +1,9 @@
 var searchData=
 [
-  ['get_5fword_0',['get_word',['../newwordle_8h.html#a9b17de3ad06f814ec1d3652d941f6961',1,'newwordle.cpp']]],
-  ['getdifficulty_1',['getDifficulty',['../secretNumber_8h.html#af7b4df3e82af155a8d9194fb4f74bbbb',1,'secretNumber.cpp']]],
-  ['getguess_2',['getGuess',['../secretNumber_8h.html#a844e898309a8fbfee9f8e9ceb0ab21f0',1,'secretNumber.cpp']]],
-  ['getsecretnumber_3',['getSecretNumber',['../secretNumber_8h.html#a318f35b6d5a6e2387867b2aa93880df0',1,'secretNumber.cpp']]]
+  ['gamereset_0',['gamereset',['../newwordle_8h.html#a4d57cce06f04673a691f1614c5116713',1,'newwordle.cpp']]],
+  ['get_5fword_1',['get_word',['../newwordle_8h.html#a9b17de3ad06f814ec1d3652d941f6961',1,'newwordle.cpp']]],
+  ['getcolourblindess_2',['getcolourblindess',['../newwordle_8h.html#a579642e7a7bdd69c0fec687cd2866034',1,'newwordle.cpp']]],
+  ['getdifficulty_3',['getDifficulty',['../secretNumber_8h.html#af7b4df3e82af155a8d9194fb4f74bbbb',1,'secretNumber.cpp']]],
+  ['getguess_4',['getGuess',['../secretNumber_8h.html#a844e898309a8fbfee9f8e9ceb0ab21f0',1,'secretNumber.cpp']]],
+  ['getsecretnumber_5',['getSecretNumber',['../secretNumber_8h.html#a318f35b6d5a6e2387867b2aa93880df0',1,'secretNumber.cpp']]]
 ];
